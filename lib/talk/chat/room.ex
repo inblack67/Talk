2f4aca3953076b00bd9985041bde4aa1c75e2a69
aman <aman>
2021-11-09 20:28:21 +0530

    list rooms

--- lib/talk/chat/room.ex
+++ lib/talk/chat/room.ex
@@ -1,7 +1,9 @@
 defmodule Talk.Chat.Room do
   use Ecto.Schema
   import Ecto.Changeset
+  require Protocol
 
+  @derive {Jason.Encoder, except: [:__meta__]}
   schema "rooms" do
     field :description, :string
     field :name, :string
@@ -13,6 +15,7 @@ defmodule Talk.Chat.Room do
   def changeset(room, attrs) do
     room
     |> cast(attrs, [:name, :description])
+    |> unique_constraint(:name)
     |> validate_required([:name, :description])
     |> validate_length(:name, min: 5, max: 20)
     |> validate_length(:description, min: 15, max: 300)
--- lib/talk_web/controllers/room_controller.ex
+++ lib/talk_web/controllers/room_controller.ex
@@ -1,13 +1,32 @@
 defmodule TalkWeb.RoomController do
   use TalkWeb, :controller
   alias Talk.Chat
+  alias TalkWeb.Utils
+  alias TalkWeb.Constants
 
   def add(conn, params) do
-    IO.inspect(params)
-    conn
+    case Chat.create_room(params) do
+      {:ok, _room} ->
+        conn |> render("acknowledge.json", %{message: "Room created"})
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn
+        |> render("errors.json", %{
+          errors:
+            changeset
+            |> Utils.format_changeset_errors()
+        })
+
+      _ ->
+        conn
+        |> render("errors.json", %{errors: [Constants.internal_server_error()]})
+    end
   end
 
-  def get_all(conn, _params), do: conn |> render("data.json", %{data: Chat.list_rooms()})
+  def get_all(conn, _params) do
+    conn
+    |> render("data.json", data: Chat.list_rooms())
+  end
 
   def greet(conn, _params) do
     conn |> render("acknowledge.json", %{message: "API up and running"})
