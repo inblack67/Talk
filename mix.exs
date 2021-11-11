70ff9db98f8e38ea791e44bb41cee4aab6bbe046
aman <aman>
2021-11-11 22:43:59 +0530

    elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Talk.MixProject do
     [
       app: :talk,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
